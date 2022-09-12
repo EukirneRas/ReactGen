c69346b04438a8485ac178ee25933d57d4486267
EukirneRas <henriquedeandraderodrigues@gmail.com>
2022-09-12 14:15:50 -0300

    blog-pessoal

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Grid} from '@material-ui/core';
+import "./Home.css"
+
+function Home(){
+    return(
+        <div>
+            <h1>Home</h1>
+            <h2>Teste2</h2>
+        </div>
+    );
+}
+
+export default Home;
